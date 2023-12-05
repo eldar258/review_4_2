47a678dc25ff722f048d72cd2897989e0104299f
Eldar <9eldik7@gmail.com>
2023-12-05 21:50:49 +0300

    app: change CreateRepository to CreateDAO

--- app/app.go
+++ app/app.go
@@ -3,7 +3,6 @@ package app
 import (
 	"log"
 	"studentgit.kata.academy/eldar/review_4_2/migration"
-	"studentgit.kata.academy/eldar/review_4_2/repository"
 	"studentgit.kata.academy/eldar/review_4_2/storage"
 	"studentgit.kata.academy/eldar/review_4_2/storage/mysql"
 )
@@ -11,7 +10,7 @@ import (
 type App struct {
 }
 
-func (a *App) CreateRepository() (repository.Repositorer, error) {
+func (a *App) CreateDAO() (storage.DAO, error) {
 	sqlx, err := mysql.Connect()
 	if err != nil {
 		log.Printf("app.CreateRepository().mysql.Connect() err: %s", err)
@@ -22,7 +21,7 @@ func (a *App) CreateRepository() (repository.Repositorer, error) {
 		log.Printf("app.CreateRepository().migration.Migrate(sqlx) err: %s", err)
 		return nil, err
 	}
-	dao := storage.New(sqlx)
 
-	return repository.New(dao), nil
+	dao := storage.New(sqlx)
+	return dao, nil
 }
