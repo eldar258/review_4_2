781058e151fb4fdeb0542812f2f570312a3c7317
Eldar <9eldik7@gmail.com>
2023-12-05 21:53:13 +0300

    migration: create city with id SERIAL type

--- migration/migration.go
+++ migration/migration.go
@@ -4,7 +4,7 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-const createCities = `CREATE TABLE IF NOT EXISTS cities ( id INTEGER NOT NULL PRIMARY KEY, name VARCHAR(30) NOT NULL, state VARCHAR(30) NOT NULL )`
+const createCities = `CREATE TABLE IF NOT EXISTS cities ( id SERIAL PRIMARY KEY, name VARCHAR(30) NOT NULL, state VARCHAR(30) NOT NULL )`
 
 func Migrate(db *sqlx.DB) error {
 	_, err := db.Exec(createCities)
