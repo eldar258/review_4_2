905916284cd765fe5a732cfe87d8b9a03aba93fa
Eldar <9eldik7@gmail.com>
2023-12-05 21:08:08 +0300

    app: add migration statement

--- app/app.go
+++ app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"log"
+	"studentgit.kata.academy/eldar/review_4_2/migration"
 	"studentgit.kata.academy/eldar/review_4_2/repository"
 	"studentgit.kata.academy/eldar/review_4_2/storage"
 	"studentgit.kata.academy/eldar/review_4_2/storage/mysql"
@@ -17,6 +18,10 @@ func (a *App) CreateRepository() (repository.Repositorer, error) {
 		return nil, err
 	}
 
+	if err = migration.Migrate(sqlx); err != nil {
+		log.Printf("app.CreateRepository().migration.Migrate(sqlx) err: %s", err)
+		return nil, err
+	}
 	dao := storage.New(sqlx)
 
 	return repository.New(dao), nil
---
+++ migration/migration.go
@@ -0,0 +1,12 @@
+package migration
+
+import (
+	"github.com/jmoiron/sqlx"
+)
+
+const createCities = `CREATE TABLE IF NOT EXISTS cities ( id INTEGER NOT NULL PRIMARY KEY, name VARCHAR(30) NOT NULL, state VARCHAR(30) NOT NULL )`
+
+func Migrate(db *sqlx.DB) error {
+	_, err := db.Exec(createCities)
+	return err
+}
