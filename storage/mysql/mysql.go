eb4b9b903f9a7783400d300d429de8b06ebf6dab
Eldar <9eldik7@gmail.com>
2023-12-05 21:08:48 +0300

    mysql: connect to db logic

---
+++ storage/mysql/mysql.go
@@ -0,0 +1,10 @@
+package mysql
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+func Connect() (*sqlx.DB, error) {
+	return sqlx.Connect("mysql", "test:test@(localhost:3306)/test")
+}
